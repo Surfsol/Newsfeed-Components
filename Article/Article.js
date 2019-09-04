afdf714ce632ae156c21d36fe26a023eb678da21
unknown <surfsol@protonmail.com>
2019-09-05 07:19:12 +0800

    mvp

--- Article/Article.js
+++ Article/Article.js
@@ -3,7 +3,7 @@
 const data = [
   {
     title: 'Lambda School Students: "We\'re the best!"',
-    date: 'Nov 5th, 2018',
+    date: "Nov 5th, 2018",
     firstParagraph: `Lucas ipsum dolor sit amet ben twi'lek padmé darth darth darth moff hutt organa twi'lek. Ben amidala secura skywalker lando
         moff wicket tatooine luke.Solo wampa wampa calrissian yoda moff.Darth grievous darth gonk darth hutt.Darth baba skywalker
         watto fett jango maul han.Mon ewok sidious sidious lando kenobi grievous gamorrean solo.Yoda wedge utapau darth calamari.
@@ -23,8 +23,8 @@ const data = [
         moff calamari mon obi-wan. Solo grievous lando coruscant. Jinn darth palpatine obi-wan mon.`
   },
   {
-    title: 'Javascript and You, ES6',
-    date: 'May 7th, 2019',
+    title: "Javascript and You, ES6",
+    date: "May 7th, 2019",
     firstParagraph: `Alohamora wand elf parchment, Wingardium Leviosa hippogriff, house dementors betrayal. Holly, Snape centaur portkey ghost
         Hermione spell bezoar Scabbers. Peruvian-Night-Powder werewolf, Dobby pear-tickle half-moon-glasses, Knight-Bus. Padfoot
         snargaluff seeker: Hagrid broomstick mischief managed. Snitch Fluffy rock-cake, 9 ¾ dress robes I must not tell lies. Mudbloods
@@ -43,8 +43,8 @@ const data = [
         sing above the ground, Ginny Weasley bright red. Fanged frisbees, phoenix tears good clean match.`
   },
   {
-    title: 'React vs Angular vs Vue',
-    date: 'June 7th, 2019',
+    title: "React vs Angular vs Vue",
+    date: "June 7th, 2019",
     firstParagraph: `Bulbasaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ivysaur Lorem ipsum dolor sit amet, consectetur adipiscing
         elit. Venusaur Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charmander Lorem ipsum dolor sit amet, consectetur
         adipiscing elit. Charmeleon Lorem ipsum dolor sit amet, consectetur adipiscing elit. Charizard Lorem ipsum dolor sit amet,
@@ -71,8 +71,24 @@ const data = [
         Castform Lotad the power that's inside Burnt Berry Makuhita. Ghost Ariados Corphish Dusclops Golbat Gligar Zweilous.`
   },
   {
-    title: 'Professional Software Development in 2019',
-    date: 'Jan 1st, 2019',
+    title: "Professional Software Development in 2019",
+    date: "Jan 1st, 2019",
+    firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
+          hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
+          Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
+
+    secondParagraph: `Hodor, hodor. Hodor. Hodor, hodor, hodor. Hodor hodor, hodor. Hodor hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor;
+          hodor hodor hodor? Hodor, hodor. Hodor. Hodor, hodor - HODOR hodor, hodor hodor hodor! Hodor, hodor. Hodor. Hodor, HODOR
+          hodor, hodor hodor, hodor, hodor hodor. Hodor hodor - hodor - hodor... Hodor hodor hodor hodor hodor hodor hodor?! Hodor
+          hodor - hodor hodor hodor. Hodor. Hodor hodor... Hodor hodor hodor hodor hodor? `,
+
+    thirdParagraph: `Hodor hodor - hodor... Hodor hodor hodor hodor. Hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor; hodor hodor? Hodor!
+          Hodor hodor, HODOR hodor, hodor hodor?! Hodor! Hodor hodor, HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor.
+          Hodor. Hodor, hodor, hodor. Hodor hodor... Hodor hodor hodor?! Hodor, hodor... Hodor hodor HODOR hodor, hodor hodor. Hodor.`
+  },
+  {
+    title: "Software Development in 2019",
+    date: "Feb 1st, 2019",
     firstParagraph: `Hodor hodor HODOR! Hodor hodor - hodor, hodor. Hodor hodor... Hodor hodor hodor; hodor hodor. Hodor hodor hodor, hodor, hodor
           hodor. Hodor, hodor. Hodor. Hodor, hodor - hodor... Hodor hodor hodor; hodor HODOR hodor, hodor hodor?! Hodor hodor, hodor.
           Hodor hodor hodor hodor hodor! Hodor hodor - HODOR hodor, hodor hodor hodor hodor hodor; hodor hodor? `,
@@ -110,5 +126,81 @@ const data = [
   Step 4: Map over the data, creating a component for each oject and add each component to the DOM as children of the 'articles' div.
 
   Step 5: Add a new article to the array. Make sure it is in the same format as the others. Refresh the page to see the new artible
+
+*/
+
+function articleCreator(
+  title,
+  date,
+  firstParagraph,
+  secondParagraph,
+  thirdParagraph
+) {
+  //define elements
+  let articleFeed = document.createElement("div");
+  let articleTitle = document.createElement("h2");
+  let articleDate = document.createElement("p");
+  let article1 = document.createElement("p");
+  let article2 = document.createElement("p");
+  let article3 = document.createElement("p");
+  let articleButton = document.createElement("span");
+
+  //add classes
+  articleFeed.classList.add("article");
+  articleDate.classList.add("date");
+  articleButton.classList.add("expandButton");
+
+  //set content
+  articleTitle.textContent = title;
+  articleDate.textContent = date;
+  article1.textContent = firstParagraph;
+  article2.textContent = secondParagraph;
+  article3.textContent = thirdParagraph;
+  articleButton.textContent = "Push Me";
+  articleButton.style.background = "blue";
+
+  //append content
+  articleFeed.appendChild(articleTitle);
+  articleFeed.appendChild(articleDate);
+  articleFeed.appendChild(article1);
+  articleFeed.appendChild(article2);
+  articleFeed.appendChild(article3);
+  articleFeed.appendChild(articleButton);
+
+  //eventListener
+  articleButton.addEventListener("click", e => {
+    articleFeed.classList.toggle("article-open");
+  });
+  return articleFeed;
+}
+window.addEventListener("load", () => {
+  let allArticles = document.querySelector(".articles");
+
+  data.map(e => {
+    console.log(e);
+    allArticles.appendChild(
+      articleCreator(
+        e.title,
+        e.date,
+        e.firstParagraph,
+        e.secondParagraph,
+        e.thirdParagraph
+      )
+    );
+  });
+  /*
+  console.log(articleArray);
+
+  articleArray.forEach(item => {
+    allArticles.appendChild(item);
+  });*/
+});
+
+/*
+data.forEach( e =>{
+  allArticles.appendChild(articleCreator(e.title,
+    e.date, e.firstParagraph, e.secondParagraph,
+     e.thirdParagraph))
+})
 
 */
--- Menu/Menu.js
+++ Menu/Menu.js
@@ -1,12 +1,12 @@
 /* This is the data we will be using, study it but don't change anything, yet. */
 
 let menuItems = [
-  'Students',
-  'Faculty',
+  "Students",
+  "Faculty",
   "What's New",
-  'Tech Trends',
-  'Music',
-  'Log Out'
+  "Tech Trends",
+  "Music",
+  "Log Out"
 ];
 
 /* 
@@ -32,4 +32,56 @@ let menuItems = [
 
   Step 6: add the menu component to the DOM.
   
+*/
+
+function createMenu(array) {
+  //set elemetns
+  let menuDiv = document.createElement("div");
+  let uL = document.createElement("ul");
+  let li1 = document.createElement("li");
+  let li2 = document.createElement("li");
+  let li3 = document.createElement("li");
+  let li4 = document.createElement("li");
+  let li5 = document.createElement("li");
+  let li6 = document.createElement("li");
+
+  //set class
+  menuDiv.classList.add("menu");
+
+  //append
+  menuDiv.appendChild(uL);
+
+  array.forEach(e => {
+    let li = document.createElement("li");
+    ul.appendChild((li.textContent = array[array[e]]));
+  });
+
+  let menuButton = document.querySelector(".menu-button");
+  menuButton.addEventListener("click", e => {
+    menuButton.classList.toggle("menu--open");
+  });
+  return menuDiv;
+}
+
+let menuButton = document.querySelector(".menu-button");
+
+menuItems.map(e => {
+  menuButton.appendChild(createMenu(e));
+});
+
+/*
+  uL.appendChild(li1);
+  uL.appendChild(li2);
+  uL.appendChild(li3);
+  uL.appendChild(li4);
+  uL.appendChild(li5);
+  uL.appendChild(li6);
+
+  //content text
+  li1.textContent = "Students";
+  li2.textContent = "Faculty";
+  li3.textContent = "What's New";
+  li4.textContent = "Tech Trends";
+  li5.textContent = "Music";
+  li6.textContent = "Log Out";
 */
